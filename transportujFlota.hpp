663d491cd6794526ba87bf81546729fcfc4d846c
Szymon Wilczek <szymon.wilczek03@gmail.com>
2025-11-06 21:04:44 +0100

    zrobione

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,26 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int ilosc = 0;
+    int ilosc_zagle = 0;
+
+    while(ilosc<towar) {
+    if (towar==0){
+        break;
+    }
+        Stocznia stocznia{};
+        Statek* s1 = stocznia();
+        int sledzie = s1->transportuj();
+        ilosc = sledzie + ilosc;
+
+        Zaglowiec* wsk_na_zaglowiec = dynamic_cast<Zaglowiec*>(s1);
+        if (wsk_na_zaglowiec != nullptr) {
+            ilosc_zagle = ilosc_zagle + 1;
+        }
+        delete s1;
+    }
+
+    return ilosc_zagle;
 }
+
+
